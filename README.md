192506ac4acc92c5fa7d76e1aa44be0336b80591
septante <130835353+septante@users.noreply.github.com>
2025-09-19 19:40:10 +0000

    add starter code

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # minim
+
+A lightweight TUI music player.
+
+## Keybinds
+
+`Enter`: add song to queue
+
+`p`: play/pause
+
+`q`: exit
+
+`n`: skip
---
+++ flake.nix
@@ -0,0 +1,79 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      rust-overlay,
+      ...
+    }:
+    let
+      overlays = [
+        (import rust-overlay)
+        (_: prev: {
+          rust-toolchain = prev.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+        })
+      ];
+
+      systems = [
+        "x86_64-linux"
+      ];
+
+      forAllSystems =
+        f: nixpkgs.lib.genAttrs systems (system: f { pkgs = import nixpkgs { inherit system overlays; }; });
+
+      version = (builtins.fromTOML (builtins.readFile ./Cargo.toml)).package.version;
+    in
+    {
+      formatter = forAllSystems ({ pkgs }: pkgs.nixfmt-tree);
+
+      devShells = forAllSystems (
+        { pkgs }:
+        with pkgs;
+        {
+          default = mkShell rec {
+            buildInputs = [
+              # rust-bin.stable.latest.default
+              rust-toolchain
+
+              # Needed for rodio to work with ALSA
+              pkg-config
+              alsa-lib
+            ];
+
+            LD_LIBRARY_PATH = "${lib.makeLibraryPath buildInputs}";
+          };
+        }
+      );
+
+      packages = forAllSystems (
+        { pkgs }:
+        with pkgs;
+        {
+          default = rustPlatform.buildRustPackage {
+            pname = "minim";
+            src = ./.;
+            inherit version;
+
+            cargoLock = {
+              lockFile = ./Cargo.lock;
+            };
+
+            nativeBuildInputs = [
+              pkg-config
+            ];
+
+            buildInputs = [
+              alsa-lib
+            ];
+          };
+        }
+      );
+    };
+}
