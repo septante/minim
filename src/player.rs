d51262af4043ddb88427af9b622a311d5ae34941
septante <130835353+septante@users.noreply.github.com>
2025-09-20 07:58:06 +0000

    feat: populate track table and display it

--- src/player.rs
+++ src/player.rs
@@ -9,15 +9,16 @@ use crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind};
 use ratatui::{
     DefaultTerminal, Frame,
     buffer::Buffer,
-    layout::Rect,
+    layout::{Constraint, Layout, Rect},
     style::Stylize,
     symbols::border,
     text::{Line, Text},
-    widgets::{Block, Paragraph, TableState, Widget},
+    widgets::{Block, Paragraph, Row, Table, TableState, Widget},
 };
 use rodio::OutputStream;
+use walkdir::WalkDir;
 
-use crate::files::Track;
+use crate::files::{CachedField, Track};
 
 #[derive(Parser, Debug)]
 #[command(version, about)]
@@ -56,16 +57,27 @@ impl Player {
             library_root = dirs::audio_dir().ok_or(eyre!("Couldn't find music folder"))?;
         }
 
+        let tracks = Self::get_tracks_from_disk(&library_root);
+
         let mut player = Player {
             args,
             library_root,
-            tracks: Vec::new(),
+            tracks,
             exit: false,
             table_state: TableState::default(),
             _stream: stream,
         };
 
         Ok(player)
+    }
+
+    fn get_tracks_from_disk(path: &PathBuf) -> Vec<Track> {
+        let files = WalkDir::new(path)
+            .into_iter()
+            .filter_map(|e| e.ok())
+            .filter(|f| f.file_type().is_file());
+
+        files.flat_map(|f| Track::try_from(f.path())).collect()
     }
 
     pub fn run(&mut self, terminal: &mut DefaultTerminal) -> std::io::Result<()> {
@@ -76,8 +88,13 @@ impl Player {
         Ok(())
     }
 
-    fn draw(&self, frame: &mut Frame) {
-        frame.render_widget(self, frame.area());
+    fn draw(&mut self, frame: &mut Frame) {
+        let main_panel_layout =
+            &Layout::vertical([Constraint::Percentage(100), Constraint::Min(10)]);
+
+        let main_panel = main_panel_layout.split(frame.area());
+
+        self.render_table(frame, main_panel[0]);
     }
 
     fn handle_events(&mut self) -> std::io::Result<()> {
@@ -98,32 +115,52 @@ impl Player {
             _ => {}
         }
     }
-}
 
-impl Widget for &Player {
-    fn render(self, area: Rect, buf: &mut Buffer) {
-        let title = Line::from(" Minim ".bold());
-        let instructions = Line::from(vec![
-            " Play/Pause ".into(),
-            "<p>".blue().bold(),
-            " Skip ".into(),
-            "<n>".blue().bold(),
-            " Quit ".into(),
-            "<q> ".blue().bold(),
-        ]);
-        let block = Block::bordered()
-            .title(title.centered())
-            .title_bottom(instructions.centered())
-            .border_set(border::THICK);
+    fn render_table(&mut self, frame: &mut Frame, area: Rect) {
+        let rows = self.tracks.iter().map(|track| {
+            Row::new(vec![
+                track.cached_field_string(CachedField::Title),
+                track.cached_field_string(CachedField::Artist),
+                track.cached_field_string(CachedField::Duration),
+            ])
+        });
 
-        let counter_text = Text::from(vec![Line::from(vec![
-            "Library: ".into(),
-            "ABC".to_string().yellow(),
-        ])]);
+        let widths = [
+            Constraint::Percentage(50),
+            Constraint::Percentage(50),
+            Constraint::Min(9),
+        ];
 
-        Paragraph::new(counter_text)
-            .centered()
-            .block(block)
-            .render(area, buf);
+        let table = Table::new(rows, widths);
+
+        frame.render_stateful_widget(table, area, &mut self.table_state);
     }
 }
+
+// impl Widget for &Player {
+//     fn render(self, area: Rect, buf: &mut Buffer) {
+//         let title = Line::from(" Minim ".bold());
+//         let instructions = Line::from(vec![
+//             " Play/Pause ".into(),
+//             "<p>".blue().bold(),
+//             " Skip ".into(),
+//             "<n>".blue().bold(),
+//             " Quit ".into(),
+//             "<q> ".blue().bold(),
+//         ]);
+//         let block = Block::bordered()
+//             .title(title.centered())
+//             .title_bottom(instructions.centered())
+//             .border_set(border::THICK);
+
+//         let counter_text = Text::from(vec![Line::from(vec![
+//             "Library: ".into(),
+//             "ABC".to_string().yellow(),
+//         ])]);
+
+//         Paragraph::new(counter_text)
+//             .centered()
+//             .block(block)
+//             .render(area, buf);
+//     }
+// }
