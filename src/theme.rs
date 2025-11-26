23f1279dd0284d6b2f5160f4391ef11ffe0fc398
septante <130835353+septante@users.noreply.github.com>
2025-11-26 23:10:15 +0000

    feat: add initial theme file support

--- src/theme.rs
+++ src/theme.rs
@@ -1,6 +1,11 @@
+use std::{path::Path, str::FromStr};
+
+use color_eyre::eyre::{self, Result, eyre};
 use ratatui::style::Color;
+use serde::{Deserialize, Serialize};
 
 #[non_exhaustive]
+#[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
 /// Theme data for the player UI
 pub struct Theme {
     pub table_selected_row_bg_focused: Color,
@@ -11,6 +16,35 @@ pub struct Theme {
     pub progress_bar_filled: Color,
     pub sidebar_now_playing_fg: Color,
     pub sidebar_virtual_queue_fg: Color,
+}
+
+impl Theme {
+    pub fn get_theme_by_name(name: &str) -> Result<Self> {
+        let mut path = dirs::config_dir().ok_or(eyre!("Couldn't find config dir"))?;
+        path.push("minim");
+
+        path.push(name);
+
+        Self::load_from_file(path)
+    }
+
+    fn load_from_file<T>(path: T) -> Result<Self>
+    where
+        T: AsRef<Path>,
+    {
+        let s = std::fs::read_to_string(path)?;
+
+        Self::from_str(&s)
+    }
+}
+
+impl FromStr for Theme {
+    type Err = eyre::Report;
+
+    fn from_str(s: &str) -> std::result::Result<Self, Self::Err> {
+        let theme: Theme = toml::from_str(s)?;
+        Ok(theme)
+    }
 }
 
 impl Default for Theme {
@@ -25,5 +59,18 @@ impl Default for Theme {
             sidebar_now_playing_fg: Color::Blue,
             sidebar_virtual_queue_fg: Color::Magenta,
         }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn load_builtin_theme() {
+        assert_eq!(
+            Theme::default(),
+            Theme::from_str(include_str!("../assets/theme.toml")).unwrap()
+        )
     }
 }
